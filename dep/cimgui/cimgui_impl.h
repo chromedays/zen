50540d6ebd12a8ddb466b5165ac4ac107357dc04
Illkwon Ha <haik0925@gmail.com>
2019-12-01 23:03:00 -0800

    added cimgui

---
+++ dep/cimgui/cimgui_impl.h
@@ -0,0 +1,8 @@
+CIMGUI_API bool ImGui_ImplOpenGL3_Init(const char* glsl_version);
+CIMGUI_API void ImGui_ImplOpenGL3_Shutdown();
+CIMGUI_API void ImGui_ImplOpenGL3_NewFrame();
+CIMGUI_API void ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
+CIMGUI_API bool ImGui_ImplOpenGL3_CreateFontsTexture();
+CIMGUI_API void ImGui_ImplOpenGL3_DestroyFontsTexture();
+CIMGUI_API bool ImGui_ImplOpenGL3_CreateDeviceObjects();
+CIMGUI_API void ImGui_ImplOpenGL3_DestroyDeviceObjects();
