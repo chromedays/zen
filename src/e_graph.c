ea5521a991070195340328fc17e7132dbb019fdb
Illkwon Ha <haik0925@gmail.com>
2020-01-25 22:16:38 -0800

    added curve in both NLI and BB-form

--- src/e_graph.c
+++ src/e_graph.c
@@ -5,17 +5,31 @@
 #include <glad/glad.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #define MAX_DEGREE 20
+#define CURVE_POINTS_COUNT 1000
+#define POINT_RADIUS 10.f
+
+typedef struct Graph_ Graph;
+
+typedef struct Method_
+{
+    const char* name;
+    float (*call)(Graph*, float);
+} Method;
 
 typedef struct Graph_
 {
     Mesh point_mesh;
     VertexBuffer point_vb;
-    float point_radius;
 
     Mesh canvas_mesh;
     VertexBuffer canvas_vb;
+
+    FVec3 curve_points[CURVE_POINTS_COUNT];
+    uint curve_vao;
+    uint curve_vbo;
 
     GLuint unlit_shader;
 
@@ -28,65 +42,60 @@ typedef struct Graph_
     float coeffs[MAX_DEGREE + 1];
     int degree;
 
+    Method methods[2];
+    int current_method_index;
+
     int dragged_point_index;
     bool is_dragging;
 } Graph;
 
-void set_world_bound(Graph* s, const Input* input)
+int g_combs[MAX_DEGREE + 1][MAX_DEGREE + 1];
+
+static int comb(int n, int c)
+{
+    if (c == 0 || c == n)
+        return 1;
+    if (g_combs[n][c] != 0)
+        return g_combs[n][c];
+    g_combs[n][c] = comb(n - 1, c - 1) + comb(n - 1, c);
+    return g_combs[n][c];
+}
+
+static float calc_polynomial_bb(Graph* s, float t)
+{
+    float result = 0;
+    int d = s->degree;
+    for (int i = 0; i <= d; i++)
+    {
+        result += s->coeffs[i] * (float)comb(d, i) *
+                  powf(1.f - t, (float)(d - i)) * powf(t, (float)i);
+    }
+
+    return result;
+}
+
+static float
+    calc_polynomial_nli_rec(const Graph* s, int begin, int end, float t)
+{
+    if ((end - begin) == 1)
+        return (1.f - t) * s->coeffs[begin] + t * s->coeffs[end];
+
+    return (1.f - t) * calc_polynomial_nli_rec(s, begin, end - 1, t) +
+           t * calc_polynomial_nli_rec(s, begin + 1, end, t);
+}
+
+static float calc_polynomial_nli(Graph* s, float t)
+{
+    int d = s->degree;
+
+    return calc_polynomial_nli_rec(s, 0, d, t);
+}
+
+static void set_world_bound(Graph* s, const Input* input)
 {
     s->world_min = (FVec2){300, 100};
     s->world_max = (FVec2){(float)input->window_size.x - 100,
                            (float)input->window_size.y - 100};
-}
-
-EXAMPLE_INIT_FN_SIG(graph)
-{
-    Example* e = e_example_make("graph", sizeof(Graph));
-    Graph* s = (Graph*)e->scene;
-
-    s->point_mesh = rc_mesh_make_sphere(0.5f, 32, 32);
-    r_vb_init(&s->point_vb, &s->point_mesh, GL_TRIANGLES);
-    s->point_radius = 12;
-
-    s->unlit_shader = e_shader_load(e, "unlit");
-
-    s->graph_min = (FVec2){0, -3};
-    s->graph_max = (FVec2){1, 3};
-
-    set_world_bound(s, input);
-
-    {
-        Vertex canvas_vertices[4] = {
-            {{0, 0, 0}},
-            {{1, 0, 0}},
-            {{1, 1, 0}},
-            {{0, 1, 0}},
-        };
-        uint canvas_indices[6] = {0, 1, 2, 2, 3, 0};
-
-        s->canvas_mesh = rc_mesh_make_raw2(ARRAY_LENGTH(canvas_vertices),
-                                           ARRAY_LENGTH(canvas_indices),
-                                           canvas_vertices, canvas_indices);
-    }
-    r_vb_init(&s->canvas_vb, &s->canvas_mesh, GL_TRIANGLES);
-
-    s->degree = 1;
-
-    s->dragged_point_index = -1;
-
-    return e;
-}
-
-EXAMPLE_CLEANUP_FN_SIG(graph)
-{
-    Example* e = (Example*)udata;
-    Graph* s = (Graph*)e->scene;
-    glDeleteProgram(s->unlit_shader);
-    r_vb_cleanup(&s->canvas_vb);
-    rc_mesh_cleanup(&s->canvas_mesh);
-    r_vb_cleanup(&s->point_vb);
-    rc_mesh_cleanup(&s->point_mesh);
-    e_example_destroy(e);
 }
 
 static float graph_to_world_x(const Graph* s, float gx)
@@ -147,7 +156,7 @@ static int get_point_index_colliding_mouse(const Graph* s, const Input* input)
         FVec2 wmp = get_world_mouse_pos(input);
         FVec2 d = fvec2_sub(wp, wmp);
         if ((d.x * d.x + d.y * d.y) <=
-            ((s->point_radius * 1.5f) * (s->point_radius * 1.5f)))
+            (POINT_RADIUS * 1.5f) * (POINT_RADIUS * 1.5f))
         {
             index = i;
             break;
@@ -155,6 +164,71 @@ static int get_point_index_colliding_mouse(const Graph* s, const Input* input)
     }
 
     return index;
+}
+
+EXAMPLE_INIT_FN_SIG(graph)
+{
+    Example* e = e_example_make("graph", sizeof(Graph));
+    Graph* s = (Graph*)e->scene;
+
+    s->point_mesh = rc_mesh_make_sphere(0.5f, 32, 32);
+    r_vb_init(&s->point_vb, &s->point_mesh, GL_TRIANGLES);
+
+    {
+        Vertex canvas_vertices[4] = {
+            {{0, 0, 0}},
+            {{1, 0, 0}},
+            {{1, 1, 0}},
+            {{0, 1, 0}},
+        };
+        uint canvas_indices[6] = {0, 1, 2, 2, 3, 0};
+
+        s->canvas_mesh = rc_mesh_make_raw2(ARRAY_LENGTH(canvas_vertices),
+                                           ARRAY_LENGTH(canvas_indices),
+                                           canvas_vertices, canvas_indices);
+    }
+    r_vb_init(&s->canvas_vb, &s->canvas_mesh, GL_TRIANGLES);
+
+    glGenVertexArrays(1, &s->curve_vao);
+    glBindVertexArray(s->curve_vao);
+    glGenBuffers(1, &s->curve_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, s->curve_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(s->curve_points), NULL,
+                 GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(FVec3), (GLvoid*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    s->unlit_shader = e_shader_load(e, "unlit");
+
+    s->graph_min = (FVec2){0, -3};
+    s->graph_max = (FVec2){1, 3};
+
+    set_world_bound(s, input);
+
+    s->degree = 1;
+
+    s->methods[0] = (Method){"BB-form", &calc_polynomial_bb};
+    s->methods[1] = (Method){"NLI", &calc_polynomial_nli};
+
+    s->dragged_point_index = -1;
+
+    return e;
+}
+
+EXAMPLE_CLEANUP_FN_SIG(graph)
+{
+    Example* e = (Example*)udata;
+    Graph* s = (Graph*)e->scene;
+    glDeleteProgram(s->unlit_shader);
+    glDeleteBuffers(1, &s->curve_vbo);
+    glDeleteVertexArrays(1, &s->curve_vao);
+    r_vb_cleanup(&s->canvas_vb);
+    rc_mesh_cleanup(&s->canvas_mesh);
+    r_vb_cleanup(&s->point_vb);
+    rc_mesh_cleanup(&s->point_mesh);
+    e_example_destroy(e);
 }
 
 EXAMPLE_UPDATE_FN_SIG(graph)
@@ -190,6 +264,22 @@ EXAMPLE_UPDATE_FN_SIG(graph)
     igText("Degree");
     igSameLine(0, -1);
     igSliderInt("##Degree", &s->degree, 1, MAX_DEGREE, "%d");
+    igText("Method");
+    igSameLine(0, -1);
+    if (igBeginCombo("##Method", s->methods[s->current_method_index].name, 0))
+    {
+        for (int i = 0; i < ARRAY_LENGTH(s->methods); i++)
+        {
+            bool is_selected = i == s->current_method_index;
+            if (igSelectable(s->methods[i].name, is_selected, 0,
+                             (ImVec2){0, 0}))
+            {
+                s->current_method_index = i;
+            }
+        }
+        igEndCombo();
+    }
+
     igEnd();
 
     ExamplePerFrameUBO per_frame = {0};
@@ -223,20 +313,46 @@ EXAMPLE_UPDATE_FN_SIG(graph)
     int hovering_point_index = get_point_index_colliding_mouse(s, input);
     for (int i = 0; i <= s->degree; i++)
     {
-        float step = 1.f / (float)s->degree;
         FVec2 gp = get_coeff_graph_pos(s, i);
-
         FVec2 wp = graph_to_world(s, gp);
 
         ExamplePerObjectUBO per_object = {0};
         Mat4 trans = mat4_translation((FVec3){wp.x, wp.y, 0});
-        Mat4 scale = mat4_scale(s->point_radius * 2);
+        Mat4 scale = mat4_scale(POINT_RADIUS * 2.f);
         per_object.model = mat4_mul(&trans, &scale);
-        if (i == hovering_point_index)
+        if (s->is_dragging && (i == s->dragged_point_index))
+            per_object.color = (FVec3){0.6f, 0, 0};
+        else if (i == hovering_point_index)
             per_object.color = (FVec3){1, 0, 0};
         else
             per_object.color = (FVec3){0, 1, 0};
         e_apply_per_object_ubo(e, &per_object);
         r_vb_draw(&s->point_vb);
     }
+
+    for (int i = 0; i < ARRAY_LENGTH(s->curve_points); i++)
+    {
+        float step_x = 1.f / (ARRAY_LENGTH(s->curve_points) - 1);
+        float t = (float)i * step_x;
+        float p = s->methods[s->current_method_index].call(s, t);
+
+        FVec2 gp = {0};
+        gp.x = t;
+        gp.y = p;
+        FVec2 wp = graph_to_world(s, gp);
+        s->curve_points[i] = (FVec3){wp.x, wp.y, 1};
+    }
+    {
+        ExamplePerObjectUBO per_object = {0};
+        per_object.model = mat4_identity();
+        per_object.color = (FVec3){1, 0, 1};
+        e_apply_per_object_ubo(e, &per_object);
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, s->curve_vbo);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(s->curve_points),
+                    s->curve_points);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(s->curve_vao);
+    glDrawArrays(GL_LINE_STRIP, 0, ARRAY_LENGTH(s->curve_points));
+    glBindVertexArray(0);
 }
