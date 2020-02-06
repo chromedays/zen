ad8a538e4ba1b542c9cce0eb30b63805ecc7f7bb
Illkwon Ha <haik0925@gmail.com>
2020-02-05 20:19:08 -0800

    huge refactoring

--- src/e_graph.c
+++ src/e_graph.c
@@ -6,10 +6,311 @@
 #include <stdlib.h>
 #include <math.h>
 #include <float.h>
+#include <string.h>
 
 #define MAX_DEGREE 20
 #define CURVE_POINTS_COUNT 100
 #define POINT_RADIUS 10.f
+
+#if 0
+    Plotter p;
+
+    begin(&p);
+    {
+        set_axis_attribs(&(AxisAttribs){
+            .x = { .name = "X", .scale = 10, },
+            .y = { .name = "Y", .scale = 10, },
+        });
+        lines(xs, ys, &(PlotAttribs){.color = 0xFFFFFFFF});
+        points(control_xs, control_ys, &(PlotAttribs){.color = 0xFF00FFFF});
+        enable_grid(true);
+    }
+    end(&p);
+#endif
+
+typedef enum PlotType_
+{
+    PlotType_Lines,
+    PlotType_Points,
+} PlotType;
+
+typedef struct PointsBuffer_
+{
+    struct PointsBuffer_* next;
+
+    PlotType type;
+    FVec4 color;
+    float thickness;
+    FVec3* points;
+    int points_count;
+} PointsBuffer;
+
+typedef struct Axis_
+{
+    const char* name;
+    float range_min;
+    float range_max;
+} Axis;
+
+typedef struct Plotter_
+{
+    PointsBuffer* buffers;
+    Axis axes[2];
+    IVec2 canvas_size;
+    bool should_draw_grid;
+} Plotter;
+
+typedef struct AxisAttribs_
+{
+    struct
+    {
+        const char* name;
+        float range_min;
+        float range_max;
+    } attribs[2];
+} AxisAttribs;
+
+static void plt_set_axis_attribs(Plotter* p, const AxisAttribs* attribs)
+{
+    for (int axis = 0; axis < 2; axis++)
+    {
+        p->axes[axis].name = attribs->attribs[axis].name;
+        p->axes[axis].range_min = attribs->attribs[axis].range_min;
+        p->axes[axis].range_max = attribs->attribs[axis].range_max;
+    }
+}
+
+typedef struct PlotAttribs_
+{
+    FVec4 color;
+    float thickness;
+} PlotAttribs;
+
+static void plt_append_points_buffer(Plotter* p,
+                                     PlotType type,
+                                     const FVec3* points,
+                                     int points_count,
+                                     const PlotAttribs* attribs)
+{
+    uint8_t* buf =
+        (uint8_t*)malloc(sizeof(PointsBuffer) + points_count * sizeof(FVec3));
+    PointsBuffer* header = (PointsBuffer*)buf;
+    header->next = p->buffers;
+    header->type = type;
+    header->color = attribs->color;
+    header->thickness = attribs->thickness;
+    header->points = (FVec3*)(buf + sizeof(PointsBuffer));
+    header->points_count = points_count;
+    memcpy(header->points, points, points_count * sizeof(FVec3));
+    p->buffers = header;
+}
+
+static void plt_points(Plotter* p,
+                       const FVec3* points,
+                       int points_count,
+                       const PlotAttribs* attribs)
+{
+    plt_append_points_buffer(p, PlotType_Points, points, points_count, attribs);
+}
+
+static void plt_lines(Plotter* p,
+                      const FVec3* points,
+                      int points_count,
+                      const PlotAttribs* attribs)
+{
+    plt_append_points_buffer(p, PlotType_Lines, points, points_count, attribs);
+}
+
+static void plt_enable_grid(Plotter* p, bool enable)
+{
+    p->should_draw_grid = enable;
+}
+
+static void plt_init(Plotter* p)
+{
+    *p = (Plotter){0};
+    plt_set_axis_attribs(
+        p, &(AxisAttribs){
+               .attribs =
+                   {
+                       {.name = "X", .range_min = 0, .range_max = 1},
+                       {.name = "Y", .range_min = 0, .range_max = 1},
+                   },
+           });
+
+    p->canvas_size = (IVec2){100, 100};
+}
+
+static void plt_cleanup(Plotter* p)
+{
+    PointsBuffer* curr = p->buffers;
+    while (curr)
+    {
+        PointsBuffer* tmp = curr;
+        curr = curr->next;
+        free(tmp);
+    }
+    *p = (Plotter){0};
+}
+
+typedef struct PlotRenderer_
+{
+    VertexBuffer point_vb;
+
+    GLuint lines_vao;
+    GLuint lines_vbo;
+
+    GLuint unlit_shader;
+} PlotRenderer;
+
+static void plt_renderer_init(Example* e, PlotRenderer* r)
+{
+    *r = (PlotRenderer){0};
+    Mesh point_mesh = rc_mesh_make_sphere(0.5f, 32, 32);
+    r_vb_init(&r->point_vb, &point_mesh, GL_TRIANGLES);
+
+    glGenVertexArrays(1, &r->lines_vao);
+    glBindVertexArray(r->lines_vao);
+    glGenBuffers(1, &r->lines_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, r->lines_vbo);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(FVec3), (GLvoid*)0);
+
+    r->unlit_shader = e_shader_load(e, "unlit");
+}
+
+static void plt_renderer_cleanup(PlotRenderer* r)
+{
+    glDeleteProgram(r->unlit_shader);
+
+    glDeleteBuffers(1, &r->lines_vbo);
+    glDeleteVertexArrays(1, &r->lines_vao);
+    r_vb_cleanup(&r->point_vb);
+    *r = (PlotRenderer){0};
+}
+
+static void plt_draw(const Example* e, const Plotter* p, const PlotRenderer* r)
+{
+    glEnable(GL_SCISSOR_TEST);
+    glViewport(0, 0, p->canvas_size.x, p->canvas_size.y);
+    glScissor(0, 0, p->canvas_size.x, p->canvas_size.y);
+    glClearColor(0.1f, 0.1f, 0.1f, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDisable(GL_DEPTH_TEST);
+    glUseProgram(r->unlit_shader);
+
+    ExamplePerFrameUBO per_frame = {0};
+    per_frame.view =
+        mat4_lookat((FVec3){0, 0, 50}, (FVec3){0, 0, 0}, (FVec3){0, 1, 0});
+    FVec2 graph_min = {
+        p->axes[0].range_min,
+        p->axes[1].range_min,
+    };
+    FVec2 graph_max = {
+        p->axes[0].range_max,
+        p->axes[1].range_max,
+    };
+
+    FVec2 canvas_min = {0};
+    FVec2 canvas_max = {
+        (float)p->canvas_size.x,
+        (float)p->canvas_size.y,
+    };
+    Mat4 graph_to_canvas = mat4_identity();
+    graph_to_canvas.mm[0][0] =
+        (canvas_max.x - canvas_min.x) / (graph_max.x - graph_min.x);
+    graph_to_canvas.mm[3][0] =
+        (graph_max.x * canvas_min.x - canvas_max.x * graph_min.x) /
+        (graph_max.x - graph_min.x);
+    graph_to_canvas.mm[1][1] =
+        (canvas_max.y - canvas_min.y) / (graph_max.y - graph_min.y);
+    graph_to_canvas.mm[3][1] =
+        (graph_max.y * canvas_min.y - canvas_max.y * graph_min.y) /
+        (graph_max.y - graph_min.y);
+
+#if 0
+    per_frame.proj =
+        mat4_ortho(p->axes[0].range_max, p->axes[0].range_min,
+                   p->axes[1].range_max, p->axes[1].range_min, 0.1f, 100);
+#else
+    per_frame.proj = mat4_ortho(canvas_max.x, canvas_min.x, canvas_max.y,
+                                canvas_min.y, 0.1f, 100);
+    per_frame.proj = mat4_mul(&per_frame.proj, &graph_to_canvas);
+#endif
+    e_apply_per_frame_ubo(e, &per_frame);
+
+    int total_line_points_count = 0;
+    {
+        PointsBuffer* curr = p->buffers;
+        while (curr)
+        {
+            if (curr->type == PlotType_Lines)
+            {
+                total_line_points_count += curr->points_count;
+            }
+            curr = curr->next;
+        }
+    }
+
+    glBindBuffer(GL_ARRAY_BUFFER, r->lines_vbo);
+    glBufferData(GL_ARRAY_BUFFER, total_line_points_count * sizeof(FVec3), NULL,
+                 GL_DYNAMIC_DRAW);
+    {
+        int offset = 0;
+        PointsBuffer* curr = p->buffers;
+        while (curr)
+        {
+            if (curr->type == PlotType_Lines)
+            {
+                glBufferSubData(GL_ARRAY_BUFFER, offset,
+                                curr->points_count * sizeof(FVec3),
+                                curr->points);
+                offset += curr->points_count * sizeof(FVec3);
+            }
+            curr = curr->next;
+        }
+    }
+
+    glBindVertexArray(r->lines_vao);
+    glDrawArrays(GL_LINE_STRIP, 0, total_line_points_count);
+
+    {
+        FVec2 graph_size = {
+            fabsf(p->axes[0].range_max - p->axes[0].range_min),
+            fabsf(p->axes[1].range_max - p->axes[1].range_min),
+        };
+
+        PointsBuffer* curr = p->buffers;
+        while (curr != NULL)
+        {
+            if (curr->type == PlotType_Points)
+            {
+                for (int i = 0; i < curr->points_count; i++)
+                {
+                    const FVec3* point = &curr->points[i];
+                    ExamplePerObjectUBO per_object = {0};
+                    Mat4 trans = mat4_translation(*point);
+                    Mat4 scale = mat4_scalev((FVec3){
+                        curr->thickness * graph_size.x /
+                            (float)p->canvas_size.x,
+                        curr->thickness * graph_size.y /
+                            (float)p->canvas_size.y,
+                        1,
+                    });
+                    per_object.model = mat4_mul(&trans, &scale);
+                    per_object.color = (FVec3){
+                        curr->color.x,
+                        curr->color.y,
+                        curr->color.z,
+                    };
+                    e_apply_per_object_ubo(e, &per_object);
+                    r_vb_draw(&r->point_vb);
+                }
+            }
+            curr = curr->next;
+        }
+    }
+}
 
 typedef struct Graph_ Graph;
 
@@ -47,6 +348,8 @@ typedef struct Graph_
 
     int dragged_point_index;
     bool is_dragging;
+
+    PlotRenderer plot_renderer;
 } Graph;
 
 static int g_combs[MAX_DEGREE + 1][MAX_DEGREE + 1];
@@ -245,6 +548,8 @@ EXAMPLE_INIT_FN_SIG(graph)
 
     s->dragged_point_index = -1;
 
+    plt_renderer_init(e, &s->plot_renderer);
+
     return e;
 }
 
@@ -252,6 +557,9 @@ EXAMPLE_CLEANUP_FN_SIG(graph)
 {
     Example* e = (Example*)udata;
     Graph* s = (Graph*)e->scene;
+
+    plt_renderer_cleanup(&s->plot_renderer);
+
     glDeleteProgram(s->unlit_shader);
     glDeleteBuffers(1, &s->curve_vbo);
     glDeleteVertexArrays(1, &s->curve_vao);
@@ -352,6 +660,7 @@ EXAMPLE_UPDATE_FN_SIG(graph)
         igEnd();
     }
 
+#if 0
     ExamplePerFrameUBO per_frame = {0};
     per_frame.view =
         mat4_lookat((FVec3){0, 0, 50}, (FVec3){0, 0, 0}, (FVec3){0, 1, 0});
@@ -421,4 +730,53 @@ EXAMPLE_UPDATE_FN_SIG(graph)
     glBindVertexArray(s->curve_vao);
     glDrawArrays(GL_LINE_STRIP, 0, ARRAY_LENGTH(s->curve_points));
     glBindVertexArray(0);
+#endif
+
+    Plotter plotter;
+    plt_init(&plotter);
+    plt_set_axis_attribs(&plotter, &(AxisAttribs){
+                                       .attribs = {{
+                                                       .name = "X",
+                                                       .range_min = 0,
+                                                       .range_max = 1,
+                                                   },
+                                                   {
+                                                       .name = "Y",
+                                                       .range_min = -1,
+                                                       .range_max = 1,
+                                                   }},
+                                   });
+
+    for (int i = 0; i < ARRAY_LENGTH(s->curve_points); i++)
+    {
+        float step_x = 1.f / (ARRAY_LENGTH(s->curve_points) - 1);
+        float t = (float)i * step_x;
+        float p = s->methods[s->current_method_index].call(s, t);
+
+        FVec2 gp = {0};
+        gp.x = t;
+        gp.y = p;
+        // FVec2 wp = graph_to_world(s, gp);
+        s->curve_points[i] = (FVec3){gp.x, gp.y, 0};
+    }
+
+    plt_set_axis_attribs(
+        &plotter, &(AxisAttribs){
+                      .attribs =
+                          {
+                              {.name = "X", .range_min = 0, .range_max = 1},
+                              {.name = "Y", .range_min = -3, .range_max = 3},
+                          },
+                  });
+
+    plt_points(&plotter, s->curve_points, ARRAY_LENGTH(s->curve_points),
+               &(PlotAttribs){
+                   .color = (FVec4){1, 0, 1, 1},
+                   .thickness = 10,
+               });
+
+    plotter.canvas_size = input->window_size;
+    plt_draw(e, &plotter, &s->plot_renderer);
+
+    plt_cleanup(&plotter);
 }
